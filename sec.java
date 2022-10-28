05a773c0d18fd3a374e06012a86cfadf0936fc71
AYUSH SINGODIA <91623298+Ayush-Singodia@users.noreply.github.com>
2022-10-28 12:11:35 +0530

    Create sec.java
    Basic java code to undwerstant about data types and vairables

---
+++ sec.java
@@ -0,0 +1,21 @@
+// This is a basic programm to understand about the data type and variables in java 
+// contribute by Ayush 
+
+public class sec{
+ public static void main(String[]args)
+ {  
+  
+   int a =5;
+   float b=4.5;
+   char c='t';
+  
+   //First statement is print the integar value
+   System.out.println("The value of a is:\n" +a);
+   
+   //Second statement to print the float value
+   System.out.println("The value of b is:\n" +b);
+   
+   //Third value to print the character value 
+   System.out.println("The value of c is:\n" +c);
+ }
+}

a4973e7beb9954ceece410440f2757dd189aa9d2
AYUSH SINGODIA <91623298+Ayush-Singodia@users.noreply.github.com>
2022-11-23 16:17:06 +0530

    Create Third.java
    This documention helps you understand the basics of java

---
+++ Third.java
@@ -0,0 +1,21 @@
+// basic code to understand about operators such like that arithmetic ,assignment,addition ,subtraction,division nd many more...
+public class third {
+  public static void main(String args[]) {
+
+    int a = 2;
+    int b = 3;
+    
+    //add of 2 numbers
+    int c = a + b;
+   
+    //multipication of 2 numbers
+    int d = a * b;
+    
+    //division of 2 numbers
+    int n = a / b;
+    System.out.println("The addition of a and b is:" + c);
+    System.out.println("The mmulti " + d);
+    System.out.println("The mmulti " + n);
+
+  }
+}

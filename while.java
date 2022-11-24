53711e5af4454e4ca5190ce80002bd560c57d4d5
AYUSH SINGODIA <91623298+Ayush-Singodia@users.noreply.github.com>
2022-11-24 20:05:08 +0530

    Create while.java
    Basic java code to understand while loops in the easiest way...

---
+++ while.java
@@ -0,0 +1,15 @@
+//This is a. basic code to understand about while loop in easiest way ....
+public class whiles{
+    public static void main(String[]args)
+     {   System.out.println("This a basic while loop programee\n");
+        int i=1;
+      
+      //step-1 use while key word 
+      //step-2 then use() and assign a condtion 
+       while(i<=5)
+       {
+        System.out.println("*\n");
+        i++;
+       } 
+    }
+}

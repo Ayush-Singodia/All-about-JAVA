b6f95513d0c8b3c137fb7bb1ecdd5fa8cf47703a
AYUSH SINGODIA <91623298+Ayush-Singodia@users.noreply.github.com>
2023-01-25 14:08:51 +0530

    ifelselader.java
    This is the basic code to understand the if-else leader conditional statement!

---
+++ ifelselader.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//if else lader
+public class sevent{
+    public static void main(String[]args){
+        System.out.println("Enter a number\n");
+   Scanner scn = new Scanner(System.in);
+    int a =scn.nextInt();
+     
+    if(a<15)
+    System.out.println("num is less then 15\n");
+    else if(a<20)
+    System.out.println("Num is less then 20\n");
+    else if(a<25)
+    System.out.println("Num is less then 25\n");
+
+    else
+    System.out.println("Num is less then 14\n");
+
+
+    }
+    
+}

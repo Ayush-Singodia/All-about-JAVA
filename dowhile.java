2ab77069691ceef66b0e02d835071ba702696e16
AYUSH SINGODIA <91623298+Ayush-Singodia@users.noreply.github.com>
2023-01-26 23:11:38 +0530

    while.java
    This is the basic code to understand the while loop in java in the easiest way

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class dowhil{
+    public static void main(String[]args){
+        int i=0;
+        do{
+            System.out.println(i);
+            i=i+1;
+        }while(i<11);
+    }
+
+}

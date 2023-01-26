6c5fc11662a16af8168266adf82f2487551fdde4
AYUSH SINGODIA <91623298+Ayush-Singodia@users.noreply.github.com>
2023-01-26 23:13:45 +0530

    table.java
    //basic code for print table of insert number

---
+++ table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+//basic code for print table of insert number
+class pr2loop{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+  
+        for(int i=1; i<11; i++){
+            System.out.println(i*n);
+        }
+
+        
+    }
+}

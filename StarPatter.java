f3c5e3a17f92fcfdb10307e0b41f30a895d1e5b9
Beast-Aayush <91623298+Ayush-Singodia@users.noreply.github.com>
2023-07-07 16:22:43 +0530

    Create StarPatter.java
    Java code to print star pattern of 4 x 5

---
+++ StarPatter.java
@@ -0,0 +1,18 @@
+public class forlooppr2 {
+    public static void main(String args[]) {
+
+        // Java program to print a star pattern
+        // with the use of for loops
+       
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
+

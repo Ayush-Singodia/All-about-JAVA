b247832c7c3a5cde0b81d5a1882815531b5ed751
Beast-Aayush <91623298+Ayush-Singodia@users.noreply.github.com>
2023-07-12 22:07:32 +0530

    Create functionstring.java

---
+++ functionstring.java
@@ -0,0 +1,15 @@
+public class funstring {
+    public static void main(String[]args){
+        String message = greet();
+        System.out.println(message);
+
+    }
+
+    //string function which basically means ,it's returning a string data type
+
+    static String greet() {
+        String greeting = "how are you ";   
+        return greeting;
+    }
+    
+}
